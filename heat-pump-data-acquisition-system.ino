57739590a1b04f590139194a364205d6e56ac57f
Piotr K. Krzemiński <p.krzeminski7@gmail.com>
2025-03-05 14:15:31 +0100

    Swap dfrobot aht20 with plain aht20

--- heat-pump-data-acquisition-system.ino
+++ heat-pump-data-acquisition-system.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <DFRobot_AHT20.h>
+#include <AHT20.h>
+#include <Wire.h>
 #include <DFRobot_I2C_Multiplexer.h>
 #include <DFRobot_RGBLCD1602.h>
 #include <GravityRtc.h>
@@ -11,17 +12,28 @@
 #define TIME_OF_SPLASH_SCREEN 2000
 #define TIME_BETWEEN_ERROR_RETRY 1200
 #define TIME_BETWEEN_LED_BLINK 1000
-#define TIME_BETWEEN_MEASUREMENTS 1200000
-#define TIME_BETWEEN_LCD_CYCLES 3000
+#define TIME_BETWEEN_MEASUREMENTS 3600000
+#define TIME_BETWEEN_LCD_CYCLES 2000
 
-#define AHT20_COUNT 1
+// Sensors
+#define AHT20_COUNT 8
 #define MUX_AHT20_FIRST 0
 
 #define LCD_ROWS 2
 
 ///////////////
-#define DEBUG 1
+#define DEBUG 0
 ///////////////
+
+// Own code
+#include "src/Logger.h"
+#include "src/Scheduler.h"
+
+#if DEBUG
+Logger logger(57600);  // 256
+#else
+Logger logger(57600);
+#endif
 
 #if DEBUG
 #define DEBUG_PRINT(...)       \
@@ -35,20 +47,10 @@
 #define DEBUG_PRINTLN(...)
 #endif
 
-// Own code
-#include "src/Logger.h"
-#include "src/Scheduler.h"
-
 DFRobot_I2C_Multiplexer I2CMulti(&Wire, 0x70);
-DFRobot_AHT20 aht20[AHT20_COUNT];
+AHT20 aht20[AHT20_COUNT];
 DFRobot_RGBLCD1602 lcd(0x2D, 16, 2);
 GravityRtc rtc;
-
-#if DEBUG
-Logger logger(57600);  // 256
-#else
-Logger logger(57600);
-#endif
 
 bool builtInLedState = false;
 
@@ -80,6 +82,9 @@ void setup() {
 
     DEBUG_PRINTLN("Starting...");
 
+    // For I2C, aht needs it explicitly
+    Wire.begin();
+
     // I2C Multiplexer related
     DEBUG_PRINT("I2C Multiplexer initialization... ");
     I2CMulti.begin();
@@ -110,8 +115,8 @@ void setup() {
         DEBUG_PRINT(" sensor initialization... ");
 
         I2CMulti.selectPort(MUX_AHT20_FIRST + i);
-        while ((status = aht20[i].begin()) != 0) {
-            DEBUG_PRINT("failed. error status: ");
+        while ((status = aht20[i].begin()) == 0) {
+            DEBUG_PRINTLN("failed. error status: ");
             DEBUG_PRINTLN(status);
 
             delay(TIME_BETWEEN_ERROR_RETRY);
@@ -124,7 +129,7 @@ void setup() {
     DEBUG_PRINT("RTC initialization... ");
 
     rtc.setup();
-    rtc.adjustRtc(F(__DATE__), F(__TIME__)); // Run once to set the RTC time
+//    rtc.adjustRtc(F(__DATE__), F(__TIME__)); // Run once to set the RTC time
 
     DEBUG_PRINTLN("done.");
 
@@ -175,10 +180,10 @@ void measure() {
     for (int i = 0; i < AHT20_COUNT; i++) {
         I2CMulti.selectPort(MUX_AHT20_FIRST + i);
 
-        if (aht20[i].startMeasurementReady(true)) {
+        if (aht20[i].triggerMeasurement()) {
             // Convert float to string with 2 decimal places
-            dtostrf(aht20[i].getTemperature_C(), 6, 2, tempStr);
-            dtostrf(aht20[i].getHumidity_RH(), 6, 2, humStr);
+            dtostrf(aht20[i].getTemperature(), 6, 2, tempStr);
+            dtostrf(aht20[i].getHumidity(), 6, 2, humStr);
 
             // Format the entire message into a buffer
             sprintf(buffer, ", %s, %s", tempStr, humStr);
@@ -203,8 +208,8 @@ void display() {
 
         I2CMulti.selectPort(MUX_AHT20_FIRST + sensorIndex);
         char tempStr[6], humStr[6], buffer[16];
-        dtostrf(aht20[sensorIndex].getTemperature_C(), 3, 1, tempStr);
-        dtostrf(aht20[sensorIndex].getHumidity_RH(), 3, 1, humStr);
+        dtostrf(aht20[sensorIndex].getTemperature(), 3, 1, tempStr);
+        dtostrf(aht20[sensorIndex].getHumidity(), 3, 1, humStr);
         sprintf(buffer, "%d: %sC %s%%", sensorIndex, tempStr, humStr);
 
         lcd.setCursor(0, i);
